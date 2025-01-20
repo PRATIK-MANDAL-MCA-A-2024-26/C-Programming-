5ffbebac729ca81f7bd7fa6c23536afaf3201cbe
PRATIK_MANDAL_MCA_A_2024-26_ROLL-36_UEMK <pratikmandal139129@gmail.com>
2025-01-20 11:13:18 +0530

    Create Fibonacci Series.c

---
+++ Assignment -1/Fibonacci Series.c
@@ -0,0 +1,21 @@
+3. Write a C program to generate the first n terms of Fibonacci series.
+Source Code :
+
+#include <stdio.h>
+int main() {
+ printf("NAME: PRATIK MANDAL , MCA1A, ROLL: 36\n");
+ int i, n;
+ int t1 = 0, t2 = 1;
+ int nextTerm = t1 + t2;
+ printf("Enter the number of terms: ");
+ scanf("%d", &n);
+ printf("Fibonacci Series: %d, %d, ", t1, t2);
+ for (i = 3; i <= n; ++i) {
+ printf("%d, ", nextTerm);
+ t1 = t2;
+ t2 = nextTerm;
+ nextTerm = t1 + t2;
+ }
+ printf("\n");
+ return 0;
+}
