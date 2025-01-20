5b47c82acc41be39cd53fa15439d88f9c7670fe7
PRATIK_MANDAL_MCA_A_2024-26_ROLL-36_UEMK <pratikmandal139129@gmail.com>
2025-01-20 11:10:54 +0530

    Update Sum and average.c

--- Assignment -1/Sum and average.c
+++ Assignment -1/Sum and average.c
@@ -1,29 +1,15 @@
 1. Write a C program to find sum and average of three numbers.
-
 Source Code :
 
-#include <stdio.h>
-
-int main(){
-
- printf("NAME: PRATIK MANDAL , MCA1A, ROLL: 36\n");
-
+ #include <stdio.h>
+ int main(){
  int a, b, c, sum;
-
  float avg;
-
  printf("Enter 3 numbers: \n");
-
  scanf("%d %d %d", &a, &b, &c);
-
  sum = a + b + c;
-
  avg = sum / 3.0;
-
  printf("Sum = %d \n", sum);
-
  printf("Average = %.2f\n", avg);
-
  return 0;
-
 }
